b0c3123d00f327bdff2b7985f32559a65a1c938c
Natan <natadasilva10@gmail.com>
2021-07-05 19:11:19 -0300

    Dash

--- src/pages/admin/dashboard/chart.js
+++ src/pages/admin/dashboard/chart.js
@@ -10,13 +10,12 @@ function createData(time, amount) {
 
 const data = [
   createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
+  createData('08:00', 1),
+  createData('09:00', 3),
+  createData('12:00', 5),
+  createData('15:00', 6),
+  createData('18:00', 4),
+  createData('20:00', 3),
   createData('24:00', undefined),
 ];
 
@@ -25,7 +24,7 @@ export default function Chart() {
 
   return (
     <React.Fragment>
-      <Title>Today</Title>
+      <Title>Histórico</Title>
       <ResponsiveContainer>
         <LineChart
           data={data}
@@ -43,7 +42,7 @@ export default function Chart() {
               position="left"
               style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
             >
-              Sales ($)
+              Emails Enviados
             </Label>
           </YAxis>
           <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
